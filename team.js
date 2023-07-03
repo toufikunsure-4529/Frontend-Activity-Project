a938dd6af43401bbe930586bdbf82023453c58c0
TOUFIK SK <toufiksk.ab3@gmail.com>
2023-07-03 17:04:36 +0530

    Preview an image before it is uploaded

--- team.js
+++ team.js
@@ -1,6 +1,10 @@
 var loadFile = function(event) {
-  var image = document.getElementById('output');
-  image.src = URL.createObjectURL(event.target.files[0]);
+  var reader = new FileReader();
+  reader.onload = function(){
+    var output = document.getElementById('output');
+    output.src = reader.result;
+  };
+  reader.readAsDataURL(event.target.files[0]);
 };
 
 var uploadField = document.getElementById("myFile");
