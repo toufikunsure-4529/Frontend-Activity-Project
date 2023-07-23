d2705b07d602b873e36f86b2b1165787c7409aee
TOUFIK SK <toufiksk.ab3@gmail.com>
2023-07-23 20:07:08 +0530

    Update All page

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
-
+var pww = document.getElementById("pw");
+let num = new Date()
+pww.textContent = num;
 
 function login() {
   var errorElement = document.getElementById("error");
   var password = document.getElementById("password").value;
   var correctPassword = "Password@1234";
-  if(password === correctPassword) {
-        // window.location.href = "home.html";
-      var url = "home.html?message=success";
-      window.location.href = url;
+  if (password === correctPassword) {
+    // window.location.href = "home.html";
+    var url = "home.html?message=success";
+    window.location.href = url;
 
-      return false;    
+    return false;
   }
+  else {
+    errorElement.textContent = "Incorrect password. Please try again.";
+
+    return false; // Prevent form submission
+  }
+}
+
+let day = new Date();
+let hr = day.getHours();
+if (hr >= 0 && hr < 12) {
+  document.getElementById('wish-time').innerHTML = "Good Morning!";
+}
+else if (hr == 12) {
+  document.getElementById('wish-time').innerHTML = "Good Noon!";
+
+}
+else if (hr >= 12 && hr <= 17) {
+  document.getElementById('wish-time').innerHTML = "Good Afternoon!";
+
+}
 else {
-  errorElement.textContent = "Incorrect password. Please try again.";
-
-  return false; // Prevent form submission
-}
-}
-
-let day=new Date();
-let hr=day.getHours();
-if(hr>=0 && hr<12){
- document.getElementById('wish-time').innerHTML="Good Morning!";
-}
-else if(hr==12){
- document.getElementById('wish-time').innerHTML="Good Noon!";
+  document.getElementById('wish-time').innerHTML = "Good Evening!";
 
 }
-else if(hr>=12&&hr<=17){
- document.getElementById('wish-time').innerHTML="Good Afternoon!";
 
-}
-else{
- document.getElementById('wish-time').innerHTML="Good Evening!";
-
-}  
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var currentIPEl = document.getElementById("currentIP");
 
   // Make an HTTP request to the ipify API
   var request = new XMLHttpRequest();
   request.open("GET", "https://api.ipify.org?format=json", true);
-  request.onload = function() {
+  request.onload = function () {
     if (request.status >= 200 && request.status < 400) {
       var response = JSON.parse(request.responseText);
       var currentIP = response.ip;
@@ -51,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function() {
       currentIPEl.textContent = "Failed to retrieve IP address.";
     }
   };
-  request.onerror = function() {
+  request.onerror = function () {
     currentIPEl.textContent = "Failed to retrieve IP address.";
   };
   request.send();
