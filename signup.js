03f648ad3d61221c24618885bbad4a63797f1347
TOUFIK SK <toufiksk.ab3@gmail.com>
2023-08-07 23:25:10 +0530

    Update Signup Page

--- signup.js
+++ signup.js
@@ -1,4 +1,11 @@
-//Javascript
-let btn = document.getElementsByClassName(".btn")
-btn.addEventListener('click', alertMsg())
+let name = document.getElementById('#name')
+let usernameInput = document.getElementById("#usernameInput")
+let btn = document.querySelector('button')
+btn.addEventListener("click", showMsg)
+let pw = "Password@123"
+
+//show alert function
+function showMsg() {
+  alert(name + " Your User Name is " + usernameInput + " Password is-" + pw)
+}
 
