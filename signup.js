9e00f2b7a60154f73748bdebb5d8c4b44f23cb91
TOUFIK SK <toufiksk.ab3@gmail.com>
2023-08-08 00:22:41 +0530

    SignUp page Update

--- signup.js
+++ signup.js
@@ -1,11 +1,31 @@
-let name = document.getElementById('name')
-let usernameInput = document.getElementById("usernameInput")
-let btn = document.querySelector('button')
-btn.addEventListener("click", showMsg)
-let pw = "Password@123"
+const name = document.getElementById('name')
+const usernameInput = document.getElementById("usernameInput")
+const submitButton = document.getElementById('submitButton');
+const pw = "Password@123"
+submitButton.addEventListener("click", showMsg) // button click Event listeaner
 
-//show alert function
+// //show alert function
 function showMsg() {
-  alert(name + " Your User Name is " + usernameInput + " Password is-" + pw)
+  const userName = name.value;
+  const userEmail = usernameInput.value;
+  //User Input Must be Enter Code
+  if (userName.trim() == '') {
+    const userConfirmed = confirm(confirmMessage);
+    if (userConfirmed) {
+      displayArea.textContent = `You entered: ${item}`;
+    }
+  }
+  //After Submit Button Then show Function alert code 
+  const userConfirmed = confirm("Are Sure Correct data Input")
+  if (userConfirmed === true) {
+    alert(userName + " Your User Name is " + userEmail + " Password is-" + pw)
+  }
+  else {
+    alert("Please Check Your data and Resubmit Thank You! " + userName)
+  }
 }
+
+
+
+
 
